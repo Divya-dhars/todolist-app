d081ecfa1feec1d63b38d6e177f971661633aec7
Divya Dharshini <dravi7350@gmail.com>
2024-03-28 13:10:45 +0530

    delete Icons added and delete operation completed

--- src/Content.js
+++ src/Content.js
@@ -23,22 +23,29 @@ const Content=()=>{
         //console.log(`id ${id}`)
         const listItems=items.map((item) => item.id===id? {...item,checked:!item.checked}:item)
         setItems(listItems)
+        localStorage.setItem("todo_list",JSON.stringify(listItems))
     }
     const handleDelete=(id)=>{
         const listItems=items.filter((item)=>item.id!==id)
         setItems(listItems)
+        localStorage.setItem("todo_list",JSON.stringify(listItems))
     }
    return (
     <main>
+        {(items.length)? (
         <ul>
             {items.map((item) => (
                 <li className="item" key={item.id}>
                     <input type="checkbox"  onChange={()=>handleCheck(item.id)} checked={item.checked}/>
-                    <label>{item.item}</label>
+                    <label style={(item.checked)?{textDecoration:'line-through'}:null}onDoubleClick={()=>handleCheck(item.id)}>{item.item}</label>
                    <FaTrashAlt role="button" onClick={()=>handleDelete(item.id)} tabIndex="0" />
                 </li>
             ))}
         </ul>
+    ):(
+      <p style={{marginTop:'2rem'}}>Your list is empty</p>
+    )
+ }
     </main> 
    );
  }
